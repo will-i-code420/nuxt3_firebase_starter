69ea6110807850fda70e3e27369124a91507c316
Will Aronow <waronow123@gmail.com>
2022-12-20 13:03:39 -0700

    added vuetify 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-
+    css: ['vuetify/styles'],
+    vite: {
+        ssr: {
+            noExternal: ['vuetify'],
+        },
+    },
+    modules: [
+        async (options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config: any) => config.plugins.push(
+                vuetify()
+            ))
+        }
+    ]
 })
