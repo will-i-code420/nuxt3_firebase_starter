2f8764ecd4c6b0d1b0a322c5fc208690eda85cee
Will Aronow <waronow123@gmail.com>
2022-12-21 13:35:20 -0700

    added mobile nav and some links, v-btn with to prop is causing error

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-app-bar :elevation="2">
+      <v-app-bar-title>Application</v-app-bar-title>
+        <NuxtLink
+          v-for="(item, i) in drawerItems"
+          :key="i"
+          :to="item.link"
+          class="mx-3 hidden-sm-and-down"
+        >
+          <v-btn>{{ item.title }}</v-btn>
+        </NuxtLink>
+        <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+          class="hidden-md-and-up"
+        ></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" location="right" temporary floating>
+      <v-list nav :items="drawerItems" item-title="title" to="link">
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <NuxtWelcome />
+    </v-main>
+  </v-app>
 </template>
+
+<script setup>
+const drawer = ref(false)
+const drawerItems = reactive([
+  { icon: '', title: 'Data', link: '/data' },
+  { icon: '', title: 'About', link: '/about' },
+  { icon: '', title: 'Contact', link: '/contact' }
+])
+</script>
