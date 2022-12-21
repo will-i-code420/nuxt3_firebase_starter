dbc2c6870d78b6432b6306919ba5918dd26202cf
Will Aronow <waronow123@gmail.com>
2022-12-21 13:38:13 -0700

    created footer

--- app.vue
+++ app.vue
@@ -22,6 +22,29 @@
     <v-main>
       <NuxtWelcome />
     </v-main>
+    <v-footer
+      class="bg-indigo-lighten-1 text-center d-flex flex-column"
+    >
+      <div>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          :icon="icon"
+          variant="text"
+        ></v-btn>
+      </div>
+
+      <div class="pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </div>
+
+      <v-divider></v-divider>
+
+      <div>
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
@@ -31,5 +54,11 @@ const drawerItems = reactive([
   { icon: '', title: 'Data', link: '/data' },
   { icon: '', title: 'About', link: '/about' },
   { icon: '', title: 'Contact', link: '/contact' }
+])
+const icons = reactive([
+  'mdi-facebook',
+  'mdi-twitter',
+  'mdi-linkedin',
+  'mdi-instagram'
 ])
 </script>
